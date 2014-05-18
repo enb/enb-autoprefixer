ac589c9eb48c62edc835b2cdaba7593d209af6c9
Sofya Ilinova <tavsophi@gmail.com>
2014-05-18 23:55:42 +0400

    Removed unnecessary methods

--- techs/css-autoprefixer.js
+++ techs/css-autoprefixer.js
@@ -6,21 +6,11 @@ module.exports = require('enb/lib/build-flow').create()
     .defineOption('browserSupport')
     .target('destTarget', '?.css')
     .useSourceText('sourceTarget')
-    .methods({
-        _getOptions: function () {
-            this._browserSupport = this.getOption('browserSupport', null);
-        },
-        _getPrefixer: function () {
-            this._autoprefixer = this._browserSupport ? autoprefixer.apply(this, this._browserSupport) : autoprefixer;
-            return this._autoprefixer;
-        },
-        _getPrefixedCss: function (source) {
-            var autoprefixer = this._getPrefixer();
-            return autoprefixer.process(source).css;
-        }
-    })
     .builder(function (css) {
-        this._getOptions();
-        return this._getPrefixedCss(css);
+        var autoprefixerInstance = this._browserSupport ?
+            autoprefixer.apply(autoprefixer, this._browserSupport) :
+            autoprefixer;
+
+        return autoprefixerInstance.process(css).css;
     })
     .createTech();
