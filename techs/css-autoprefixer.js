14c8f7bc35ccefb38cf884a8d78f5efb7e750663
sbmaxx <sbmaxx@yandex-team.ru>
2015-08-11 14:36:04 +0300

    lazy require

--- techs/css-autoprefixer.js
+++ techs/css-autoprefixer.js
@@ -1,6 +1,3 @@
-var autoprefixer = require('autoprefixer-core');
-var browserslist = require('browserslist');
-
 module.exports = require('enb/lib/build-flow').create()
     .name('css-autoprefixer')
     .defineRequiredOption('sourceTarget')
@@ -11,6 +8,10 @@ module.exports = require('enb/lib/build-flow').create()
     .target('destTarget', '?.css')
     .useSourceText('sourceTarget')
     .builder(function (css) {
+
+        var autoprefixer = require('autoprefixer-core');
+        var browserslist = require('browserslist');
+
         var prefixer = autoprefixer({
             browsers: this._browserSupport || browserslist.defaults
         });
@@ -20,5 +21,6 @@ module.exports = require('enb/lib/build-flow').create()
         } catch (e) {
             throw new Error(e);
         }
+
     })
     .createTech();
